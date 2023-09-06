57ef4a6dd6d676d207e09a26ed8e9b6eccb8c7e6
isha1701 <52813797+isha1701@users.noreply.github.com>
2023-09-06 17:20:51 +0530

    Update get_pet_labels.py

--- First Project/project/get_pet_labels.py
+++ First Project/project/get_pet_labels.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
 #                                                                             
-# PROGRAMMER: Daniel Marchena Parreira
-# DATE CREATED: 3/7/2019                         
-# REVISED DATE: 3/7/2019 
+# PROGRAMMER: Amogh Joshi
+# DATE CREATED:06/09/2023                        
+# REVISED DATE: 06/09/2023
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
 #           - The Image Folder as image_dir within get_pet_labels function and 
